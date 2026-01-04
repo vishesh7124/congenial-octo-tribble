5fa6b40b686d545c4aa87d45c573ebdaa2281a2d
Aargh Rai <shubh2007rai@gmail.com>
2026-01-05 01:16:47 +0530

    incomplete upcoming events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -30,22 +30,58 @@ function rotate(new_value, total) {
 }
 
 function Event({ event }) {
-  return <div className="flex flex-col w-[60vw] gap-4 text-[#95B7DA]"><h1 className="text-8xl font-pirata">
-    {event.title}
-  </h1>
-  <h2 className="text-5xl font-gothic mb-8">
-    {event.below_title}
-  </h2>
-  <div className="flex">
-    <button onClick={() => alert('todo')} className="font-gothic text-2xl p-4 rounded-3xl" style={{
-      border: "3px solid #65C5D4",
-    }}>{"MORE INFO >"}</button>
-    <div></div>
+  return <div className="flex flex-col w-[60vw] gap-4 text-[#95B7DA] pl-32 pr-32">
+    <h1 className="text-8xl font-pirata">
+      {event.title}
+    </h1>
+    <h2 className="text-5xl font-gothic mb-8">
+      {event.below_title}
+    </h2>
+    <div className="flex">
+      <button
+        onClick={() => alert('todo')}
+        className="font-gothic text-2xl p-4 rounded-3xl cursor-pointer"
+        style={{
+          border: "3px solid #65C5D4",
+        }}
+      >{"MORE INFO >"}</button>
+      <div></div>
+    </div>
+    <h2 className="text-5xl font-gothic mt-8">Description</h2>
+    <h2 className="text-4xl font-gothic">
+      {event.description}
+    </h2>
   </div>
-  <h2 className="text-5xl font-gothic mt-8">Description</h2>
-  <h2 className="text-4xl font-gothic">
-    {event.description}
-  </h2></div>
+}
+
+const upcomingEvents = [
+  {
+    title: "Event X",
+    liner: "One liner jo bhi event hai"
+  },
+  {
+    title: "Event X",
+    liner: "One liner jo bhi event hai"
+  },
+  {
+    title: "Event X",
+    liner: "One liner jo bhi event hai"
+  },
+  {
+    title: "Event X",
+    liner: "One liner jo bhi event hai"
+  },
+]
+
+function UpcomingEvents() {
+  return <div className="flex flex-col gap-32">
+    <h2 className="section-title font-pirata text-8xl">
+      <span>Upcoming Events</span>
+    </h2>
+    <img src="/aavegh.png" alt="Aavegh" style={{
+      width: "80vw"
+    }} />
+  </div>
 }
 
 export function Events() {
@@ -53,17 +89,24 @@ export function Events() {
   return (
     <div className="min-h-screen text-pink-400">
       <TopNavOverlay />
-      <div className="pt-32 px-8 flex justify-between pt-[30vh]">
+      <div className="pt-32 pb-48 px-8 flex justify-between pt-[30vh]"
+    style={{
+      backgroundImage: "url(/aura.svg)",
+      backgroundOrigin: "padding-box",
+      backgroundSize: "cover"
+    }}
+    >
         <button
           onClick={() => setCurrentEvent(rotate(currentEvent - 1, events.length))}
-          className="text-8xl font-gothic text-[#DE87B4]" 
+          className="text-8xl font-gothic text-[#DE87B4] cursor-pointer"
         >{"<"}</button>
         <Event event={events[currentEvent]} />
         <button
           onClick={() => setCurrentEvent(rotate(currentEvent + 1, events.length))}
-          className="text-8xl font-gothic text-[#DE87B4]"
+          className="text-8xl font-gothic text-[#DE87B4] cursor-pointer"
         >{">"}</button>
       </div>
+      <UpcomingEvents />
     </div>
   );
 }
